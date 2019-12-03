4018f0f51d02b51ccaa98252eb11567557b122f0
Harrison <35613146+hmwirtz@users.noreply.github.com>
2019-12-03 01:37:51 -0800

    Reads Clumps

---
+++ Clump_Reader/Clump_Reader.cpp
@@ -0,0 +1,65 @@
+// Clump_Reader.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+string getFileName()
+{
+	string fileName;
+	cout << "Enter File name\n";
+	cin >> fileName;
+	return fileName;
+
+}
+
+bool isFileOpen()
+{
+	streampos size;
+	char* memblock;
+	string fileName;
+	fileName = getFileName();
+	std::ifstream clump;
+	clump.open(fileName, ios::in | ios::binary | ios::ate);
+	if (clump.is_open())
+	{
+		cout << "Clump loaded";
+
+		size = clump.tellg();
+		memblock = new char[size];
+		clump.seekg(0, ios::beg);
+		clump.read(memblock, size);
+
+
+		for (int i = 0; i < size; i++) {
+			unsigned int hexDec = (unsigned char)memblock[i];
+
+			cout << hex << hexDec;
+
+		}
+
+
+		clump.close();
+		return true;
+
+
+	}
+	else cout << "No clump to load";
+	return false;
+}
+
+void headerCount()
+{
+	// Count each RW header
+	// return the types of data each header uses
+
+}
+
+int main()
+{
+	cout << "HelloWorld\n";
+	isFileOpen();
+}
+
+
